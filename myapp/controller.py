9ce1742d9fba419522f11388c353cc974cadd51e
yurley <yurs.ksf1@gmail.com>
2023-02-14 15:38:09 -0500

    hot fix donwload

--- myapp/controller.py
+++ myapp/controller.py
@@ -234,7 +234,7 @@ class DataController():
             puesto_votacion_mapping[puesto_votacion.id] = {
                 "departamento": puesto_votacion.departamento.name,
                 "municipio": puesto_votacion.municipio.name,
-                "barrio": puesto_votacion.barrio.name,
+                "barrio": puesto_votacion.barrio.name if puesto_votacion.barrio else "",
                 "name": puesto_votacion.name,
                 "address": puesto_votacion.address,
             }
