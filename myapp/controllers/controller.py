92762e44849502ed2465edff4f1ae31c553ff36e
Asamblea Departamental de Santander <asambleadepartamentaldesantander@MacBook-Pro-de-Asamblea.local>
2023-08-06 19:51:45 -0500

    improve visual change and whatsapp link

--- myapp/controllers/controller.py
+++ myapp/controllers/controller.py
@@ -802,7 +802,7 @@ class DataController():
 
         data["votantes"] = votantes_list
         data["nombre"] = lider.full_name()
-        lider_profile = votante.votanteprofile_set.first()
+        lider_profile = lider.votanteprofile_set.first()
         if lider_profile:
             data["mobile_phone"] = lider_profile.mobile_phone or ""
 
