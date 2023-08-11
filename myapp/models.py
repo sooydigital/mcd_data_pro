b69a5151c53e41d99401360fc25c1e40fb0092ac
Carlos Reyes <andrespk3000@gmail.com>
2023-08-11 12:01:55 -0500

    correccion otro

--- myapp/models.py
+++ myapp/models.py
@@ -278,7 +278,7 @@ class VotanteProfile(models.Model):
     GENDER_CHOICES = [
         ("HOMBRE", "HOMBRE"),
         ("MUJER", "MUJER"),
-        ("Otro", "Otro"),
+        ("OTRO", "OTRO"),
     ]
 
     gender = models.CharField(
